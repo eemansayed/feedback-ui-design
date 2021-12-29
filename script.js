5173ac68e9938ff1a93f126f8bf86db4e0503368
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-29 11:49:00 +0000

    add const called ratingsContainer

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const ratings = document.querySelectorAll(".rating");
+const ratingsContainer = document.querySelector(".ratings-container");
 const sendBtn = document.querySelector("#send");
 const panel = document.querySelector("#panel");
 let selectedRating = "Satisfied";
