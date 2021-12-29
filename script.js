7d5f45d82b76504141d2e3f2f6f4f1417472da54
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-29 11:06:39 +0000

    add event listener called panel

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const ratings = document.querySelectorAll(".rating");
 const sendBtn = document.querySelector("#send");
 const panel = document.querySelector("#panel");
+
+panel.addEventListener("click", (e) => {
+  if (e.target.parentNode.classList.contains("rating")) {
+    removeActive();
+    e.target.parentNode.classList.add("active");
+  }
+});
